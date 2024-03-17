16923a85dd6113020270a556b2f62b28b409702c
Gab Portable <gabriel.morin98@gmail.com>
2024-03-17 16:54:09 -0400

    new file:   .gitignore 	new file:   package.json 	new file:   public/index.html 	new file:   src/index.js 	new file:   yarn.lock

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <nav>This is the nav</nav>
+    <main>This is the main section</main>
+    <footer>This is the footer</footer>
+  </>
+);
